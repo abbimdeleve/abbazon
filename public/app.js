1e8812093751795bcc9ca5a83f5de84cc43d93cd
abbimdeleve <adeleve1024@yahoo.com>
2019-08-29 18:37:05 -0400

    super great awesome finished

--- public/app.js
+++ public/app.js
@@ -6,12 +6,12 @@ $(document).ready(function() {
             if(data){
                 for (let i=0; i<data.length; i++){
                     const books = $("<div class='col'>");
-                    books.append("<p>" + data[i].book_name + "</p>");
-                    books.append("<p>" + data[i].author_name + "</p>");
-                    books.append("<img src='" + data[i].image+ "' alt='book cover'>")
+                    books.append("<p id='bookname'>" + data[i].book_name + "</p>");
+                    books.append("<p id='authorname'>" + data[i].author_name + "</p>");
+                    books.append("<img src='" + data[i].image+ "' alt='book cover' class='pics'>")
                     books.append("<p>$" + data[i].price + "</p>");
                     books.append("<p>Stock: " + data[i].stock_quantity + "</p>");
-                    books.append("<button type='button' class='btn btn-info btn-sm' id='buttonAdd'>Read it!</button>")
+                    books.append("<button type='button' class='btn btn-info btn-sm' id='buttonAdd'>Buy it!</button>")
                     books.append("<button type='button' class='btn btn-secondary btn-sm' id='buttonDel'>I can't read</button>")
                     books.append("<p>                                       </p>");
                     books.append("<p>                                       </p>");

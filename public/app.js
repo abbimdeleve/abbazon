eda84b278a6143cb1edf4fb8e17224d835a86840
abbimdeleve <adeleve1024@yahoo.com>
2019-08-29 13:15:34 -0400

    fixed styling

--- public/app.js
+++ public/app.js
@@ -11,8 +11,9 @@ $(document).ready(function() {
                     books.append("<img src='" + data[i].image+ "' alt='book cover'>")
                     books.append("<p>$" + data[i].price + "</p>");
                     books.append("<p>Stock: " + data[i].stock_quantity + "</p>");
-                    books.append("<button type='button' class='btn btn-success'>Add to Cart</button>")
-                    books.append("<button type='button' class='btn btn-danger'>Remove Item</button>")
+                    books.append("<button type='button' class='btn btn-info btn-sm' id='buttonAdd'>Read it!</button>")
+                    books.append("<button type='button' class='btn btn-secondary btn-sm' id='buttonDel'>I can't read</button>")
+                    books.append("<p>                                       </p>");
                     books.append("<p>                                       </p>");
 
                     $("#product").append(books)
@@ -21,8 +22,9 @@ $(document).ready(function() {
         })
     
     }
-
     getProducts()
+
+    
 
 
 
